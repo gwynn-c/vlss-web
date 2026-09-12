6369017040ff5ec531a5f288ecb8525850753035
mshayan3 <mshayan38@protonmail.ch>
2026-09-12 21:20:08 +0500

    Revert to /contact route (nginx routes deep paths fine)

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,4 @@ use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'home'])->name('home');
-
-// The contact form posts to "/" (handled here) so it works on nginx hosts
-// that have no Laravel front-controller rewrite for deep paths.
-Route::post('/', [ContactController::class, 'store'])->name('contact.store');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
